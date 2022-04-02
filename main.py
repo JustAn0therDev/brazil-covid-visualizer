09bb67a747c995cef7e50908843dd49e5d7894e9
Ruan Montelo <ruanmontelo@gmail.com>
2022-04-02 13:50:27 -0300

    Removing unused variable 'figure' from plt.subplots result

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ with open('owid_covid_dataset.csv', 'r') as csv:
 
     rows = list(filter(lambda x: x['location'] == 'Brazil', csvdict))
     
-    fig, ax = plt.subplots()
+    _, axis = plt.subplots()
     valid_rows = [(rows[i]["new_deaths"], rows[i]["date"][2:7]) for i in range(0, len(rows), 30) if rows[i]["date"] and rows[i]["new_deaths"]]
     dates = [int(float(i[0])) for i in valid_rows]
     new_deaths = [i[1] for i in valid_rows]
-    ax.plot(new_deaths, dates)
-    ax.set(xlabel="Data (ano-mês)", ylabel="Novas mortes", title="Visualização de novas mortes-mês")
+    axis.plot(new_deaths, dates)
+    axis.set(xlabel="Data (ano-mês)", ylabel="Novas mortes", title="Visualização de novas mortes-mês")
 
     plt.show()
 
